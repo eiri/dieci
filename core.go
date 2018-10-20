e09be0f3b5ab04a6c0311be23d0834c287dc4523
Eric Avdey <eiri@eiri.ca>
2018-10-19 23:02:19 -0300

    Super crude read and write implementation

--- core.go
+++ core.go
@@ -2,51 +2,104 @@
 package beansdb
 
 import (
+	"bytes"
+	"crypto/md5"
 	"crypto/rand"
 	"fmt"
+	"io"
 	"os"
 )
 
+// ScoreSize is the size of score in bytes
+const ScoreSize = 16
+
 // Store represents a data store.
 type Store struct {
+	idx map[[ScoreSize]byte][2]int
+	eof int
 	*os.File
-	idx map[int]int
 }
 
 // New creates a new empty storage
-func New() (s Store, err error) {
+func New() (s *Store, err error) {
 	buf := make([]byte, 16)
 	_, err = rand.Read(buf)
 	if err != nil {
 		return
 	}
-	f, err := os.Create(fmt.Sprintf("%x.data", buf))
+	storeName := fmt.Sprintf("%x.data", buf)
+	f, err := os.OpenFile(storeName, os.O_RDONLY|os.O_CREATE, 0600)
 	if err != nil {
 		return
 	}
-	s = Store{f, make(map[int]int)}
-	return
+	f.Close()
+	return Open(storeName)
 }
 
 // Open opens provided storage
-func Open(storeName string) (s Store, err error) {
-	f, err := os.Open(storeName)
+func Open(storeName string) (s *Store, err error) {
+	f, err := os.OpenFile(storeName, os.O_APPEND|os.O_RDWR, 0600)
 	if err != nil {
 		return
 	}
-	idx := make(map[int]int)
-	s = Store{f, idx}
+	// read idx
+	idx := make(map[[ScoreSize]byte][2]int)
+	pos := 0
+	buf := [ScoreSize + 1]byte{}
+	for {
+		if _, err := f.Read(buf[:]); err == io.EOF {
+			break
+		}
+		var score [ScoreSize]byte
+		copy(score[:], buf[:ScoreSize])
+		len := int(buf[ScoreSize])
+		idx[score] = [2]int{len, pos + ScoreSize + 1}
+		pos += ScoreSize + 1 + len
+		_, err = f.Seek(int64(len), 1)
+		if err != nil {
+			break
+		}
+	}
+	// reset fd and make Store
+	_, err = f.Seek(0, 0)
+	s = &Store{idx, 0, f}
 	return
 }
 
 // Read a data for a given score
-func Read() {
-	fmt.Println("Read")
+func (s *Store) Read(score [ScoreSize]byte) (b []byte, err error) {
+	val, ok := s.idx[score]
+	if !ok {
+		err = fmt.Errorf("Unknown score %x", score)
+		return
+	}
+	len := val[0]
+	pos := val[1]
+	_, err = s.File.Seek(int64(pos), 0)
+	if err != nil {
+		return
+	}
+	b = make([]byte, len)
+	_, err = s.File.Read(b)
+	return
 }
 
 // Write given data and return it's score
-func Write() {
-	fmt.Println("Write")
+func (s *Store) Write(b []byte) (score [ScoreSize]byte, err error) {
+	score = md5.Sum(b)
+	len := len(b)
+	// use buffer for now
+	buf := new(bytes.Buffer)
+	buf.Write(score[:])
+	buf.WriteByte(byte(len))
+	buf.Write(b)
+	_, err = buf.WriteTo(s.File)
+	if err != nil {
+		return
+	}
+	s.idx[score] = [2]int{len, s.eof + ScoreSize + 1}
+	s.eof += ScoreSize + 1 + len
+	return
 }
 
 // Delete provided storage
--- core_test.go
+++ core_test.go
@@ -1,14 +1,19 @@
 package beansdb_test
 
 import (
+	"crypto/rand"
 	"github.com/eiri/beansdb"
 	"os"
+	"reflect"
 	"testing"
 )
 
+type kv [2][]byte
+
+var kvs []kv
 var store, score string
 
-// TestNew to ensure we can create new storage
+// TestNew to ensure we can create a new storage
 func TestNew(t *testing.T) {
 	s, err := beansdb.New()
 	if err != nil {
@@ -22,7 +27,7 @@ func TestNew(t *testing.T) {
 	}
 }
 
-// TestOpen to ensure we can open existing storage
+// TestOpen to ensure we can open an existing storage
 func TestOpen(t *testing.T) {
 	_, err := os.Stat(store)
 	if err != nil {
@@ -35,7 +40,54 @@ func TestOpen(t *testing.T) {
 	s.Close()
 }
 
-// TestDelete to ensure we can delete existing storage
+// TestWrite to ensure we can write in the store
+func TestWrite(t *testing.T) {
+	kvs = make([]kv, 5)
+	s, err := beansdb.Open(store)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer s.Close()
+	for i, docSize := range []int{21, 12, 42, 5, 17} {
+		doc := make([]byte, docSize)
+		_, err = rand.Read(doc)
+		if err != nil {
+			t.Fatal(err)
+		}
+		score, err := s.Write(doc)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if len(score) != beansdb.ScoreSize {
+			t.Errorf("Expecting score be %d bytes long", beansdb.ScoreSize)
+		}
+		kvs[i] = kv{score[:], doc}
+		// double-write
+	}
+}
+
+// TestRead to ensure we can read from the store
+func TestRead(t *testing.T) {
+	s, err := beansdb.Open(store)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer s.Close()
+	for _, i := range [5]int{1, 2, 0, 4, 3} {
+		kv := kvs[i]
+		var score [beansdb.ScoreSize]byte
+		copy(score[:], kv[0])
+		doc, err := s.Read(score)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if !reflect.DeepEqual(doc, kv[1]) {
+			t.Error("Expecting store to return stored data")
+		}
+	}
+}
+
+// TestDelete to ensure we can delete the store
 func TestDelete(t *testing.T) {
 	_, err := os.Stat(store)
 	if err != nil {
