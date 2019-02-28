d8678fac3a3837d01b59b402c86a0e017c607758
Eric Avdey <eiri@eiri.ca>
2019-02-27 21:38:12 -0400

    Now, don't oversimplify test helpers

--- datalog_test.go
+++ datalog_test.go
@@ -13,16 +13,20 @@ import (
 
 func TestDataLog(t *testing.T) {
 	assert := require.New(t)
-	name, err := prepareDatalogFile()
+	name := randomName()
+	err := prepareDatalogFile(name)
 	assert.NoError(err)
 
 	words := "The quick brown fox jumps over the lazy dog"
 
 	t.Run("open", func(t *testing.T) {
+		missing := randomName()
+		_, err := openDataLog(missing)
+		assert.Error(err)
 		dl, err := openDataLog(name)
 		defer dl.close()
 		assert.NoError(err)
-		assert.Equal(0, dl.cur, "expecting cursor to be at 0")
+		assert.Equal(0, dl.cur, "Cursor should be on 0")
 	})
 
 	t.Run("put", func(t *testing.T) {
@@ -34,8 +38,8 @@ func TestDataLog(t *testing.T) {
 			data := []byte(word)
 			pos, size, err := dl.put(data)
 			assert.NoError(err)
-			assert.Equal(expectedPos, pos, "expecting position to move")
-			assert.Equal(pos+size, dl.cur, "expecting cursor to move")
+			assert.Equal(expectedPos, pos, "Position should move")
+			assert.Equal(pos+size, dl.cur, "Cursor should move")
 			expectedPos += size + intSize
 		}
 	})
@@ -47,7 +51,7 @@ func TestDataLog(t *testing.T) {
 		i, err := dl.Stat()
 		assert.NoError(err)
 		end := int(i.Size())
-		assert.EqualValues(end, dl.cur, "expecting cursor to be at file end")
+		assert.EqualValues(end, dl.cur, "Cursor should be at EOF")
 		pos := 0
 		for _, word := range strings.Fields(words) {
 			expectedData := []byte(word)
@@ -66,12 +70,12 @@ func TestDataLog(t *testing.T) {
 		i, err := dl.Stat()
 		assert.NoError(err)
 		end := int(i.Size())
-		assert.Equal(end, dl.cur, "expecting cursor to be at file end")
+		assert.Equal(end, dl.cur, "Cursor should be at EOF")
 		err = dl.close()
 		assert.NoError(err)
-		assert.Equal(0, dl.cur, "expecting cursor to reset")
+		assert.Equal(0, dl.cur, "Cursor should reset")
 		err = dl.close()
-		assert.Error(err, "expecting an error on a second close")
+		assert.Error(err, "Should return error on attempt to close again")
 	})
 
 	t.Run("delete", func(t *testing.T) {
@@ -79,18 +83,20 @@ func TestDataLog(t *testing.T) {
 		assert.NoError(err)
 		err = dl.delete()
 		assert.NoError(err)
-		assert.Equal(0, dl.cur, "expecting cursor to reset")
+		assert.Equal(0, dl.cur, "Cursor should reset")
 		err = dl.delete()
-		assert.Error(err, "expecting an error on a second delete")
+		assert.Error(err, "Should return error on attempt of second delete")
 	})
 }
 
-func prepareDatalogFile() (string, error) {
-	buf := make([]byte, 16)
-	rand.Read(buf)
-	name := hex.EncodeToString(buf)
-	// and create and empty datalog file
+func prepareDatalogFile(name string) error {
 	f, err := os.Create(fmt.Sprintf("%s.data", name))
 	defer f.Close()
-	return name, err
+	return err
+}
+
+func randomName() string {
+	buf := make([]byte, 16)
+	rand.Read(buf)
+	return hex.EncodeToString(buf)
 }
