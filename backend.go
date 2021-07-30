58cf9175a64fdd7dc06b6c1065975a6dc1bd1722
Eric Avdey <eiri@eiri.ca>
2021-07-30 09:28:05 -0300

    Add backend interface and BadgerDB backend implementation

---
+++ backend.go
@@ -0,0 +1,39 @@
+package dieci
+
+import (
+	badger "github.com/dgraph-io/badger/v3"
+)
+
+// Backend is an interface for Dieci backend implementation
+type Backend interface {
+	Read([]byte) ([]byte, error)
+	Write([]byte, []byte) error
+}
+
+// BadgerBackend implements backedn with BadgerDB
+type BadgerBackend struct {
+	txn *badger.Txn
+}
+
+// NewBadgerBackend returns new instance of Badger backend for a given transaction
+func NewBadgerBackend(txn *badger.Txn) Backend {
+	return &BadgerBackend{txn: txn}
+}
+
+func (bb *BadgerBackend) Read(k []byte) ([]byte, error) {
+	v := make([]byte, 0)
+	item, err := bb.txn.Get(k)
+	if err != nil {
+		return v, err
+	}
+	err = item.Value(func(val []byte) error {
+		v = append(v, val...)
+		return nil
+	})
+	return v, err
+}
+
+func (bb *BadgerBackend) Write(k []byte, v []byte) error {
+	e := badger.NewEntry(k, v)
+	return bb.txn.SetEntry(e)
+}
