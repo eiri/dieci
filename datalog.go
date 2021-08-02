1c2617c6ad09082cc0c0a04b4fead84132e82d0e
Eric Avdey <eiri@eiri.ca>
2021-08-02 18:15:33 -0300

    Extract compress/decompress into own functions

--- datalog.go
+++ datalog.go
@@ -22,25 +22,19 @@ func NewDatalog(b Backend) *Datalog {
 
 // Read is a read callback
 func (dl *Datalog) Read(score Score) ([]byte, error) {
-	b, err := dl.backend.Read(score)
+	data, err := dl.backend.Read(score)
 	if err == nil && !dl.filter.Test(score) {
 		dl.filter.Add(score)
 	}
-	// decompress
-	var data bytes.Buffer
-	r := snappy.NewReader(bytes.NewReader(b))
-	if _, err := io.Copy(&data, r); err != nil {
-		return []byte{}, err
-	}
-
-	return data.Bytes(), err
+	data1, err := decompress(data)
+	return data1, err
 }
 
 // Write is a write callback
 func (dl *Datalog) Write(data []byte) (Score, error) {
 	score := NewScore(data)
 	// if score is not in filter it's definitely not in backend
-	// if it is in filer then it's maybe in backend,
+	// if it is in filter then it's maybe in backend,
 	// so worth of read to confirm
 	if dl.filter.Test(score) {
 		if ok, err := dl.backend.Exists(score); ok {
@@ -49,20 +43,36 @@ func (dl *Datalog) Write(data []byte) (Score, error) {
 			return Score{}, err
 		}
 	}
-	// compress
-	var b bytes.Buffer
-	w := snappy.NewBufferedWriter(&b)
-	if _, err := w.Write(data); err != nil {
+	data1, err := compress(data)
+	if err != nil {
 		return Score{}, err
 	}
-	w.Close()
 	// this might be an idempotent update if this is a fresh start
 	// and score is not yet in filter
-	err := dl.backend.Write(score, b.Bytes())
+	err = dl.backend.Write(score, data1)
 	if err != nil {
 		return Score{}, err
 	}
 
 	dl.filter.Add(score)
 	return score, nil
+}
+
+func compress(data []byte) ([]byte, error) {
+	var out bytes.Buffer
+	w := snappy.NewBufferedWriter(&out)
+	if _, err := w.Write(data); err != nil {
+		return []byte{}, err
+	}
+	w.Close()
+	return out.Bytes(), nil
+}
+
+func decompress(data []byte) ([]byte, error) {
+	var out bytes.Buffer
+	r := snappy.NewReader(bytes.NewReader(data))
+	if _, err := io.Copy(&out, r); err != nil {
+		return []byte{}, err
+	}
+	return out.Bytes(), nil
 }
