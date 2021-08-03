16c7132ae0104c16f7783ca7c4bfcd5bcb6841e2
Eric Avdey <eiri@eiri.ca>
2021-08-02 21:57:43 -0300

    Switch from growing ART to proper Two-Queue cache in index

--- go.mod
+++ go.mod
@@ -7,8 +7,8 @@ require (
 	github.com/cespare/xxhash v1.1.0
 	github.com/dgraph-io/badger/v3 v3.2103.1
 	github.com/golang/snappy v0.0.3
+	github.com/hashicorp/golang-lru v0.5.4
 	github.com/muyo/sno v1.2.0
-	github.com/plar/go-adaptive-radix-tree v1.0.4
 	github.com/stretchr/testify v1.7.0
 	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9
 )
--- index.go
+++ index.go
@@ -1,34 +1,54 @@
 package dieci
 
 import (
-	art "github.com/plar/go-adaptive-radix-tree"
+	lru "github.com/hashicorp/golang-lru"
 )
 
 // cache is in memory lookup store
-type cache art.Tree
+type cache struct {
+	lru *lru.TwoQueueCache
+}
+
+func newCache(size int) *cache {
+	l, _ := lru.New2Q(size)
+	return &cache{lru: l}
+}
+
+func (c *cache) Add(key Key, score Score) {
+	cacheKey := key.String()
+	c.lru.Add(cacheKey, score)
+}
+
+func (c *cache) Get(key Key) (score Score, ok bool) {
+	cacheKey := key.String()
+	value, ok := c.lru.Get(cacheKey)
+	if ok {
+		score = value.(Score)
+	}
+	return score, ok
+}
 
 // Index represents an index of a datalog file
 type Index struct {
-	cache   cache
+	cache   *cache
 	backend Backend
 }
 
 // NewIndex returns a new index
 func NewIndex(b Backend) *Index {
-	cache := art.New()
+	cache := newCache(20000)
 	return &Index{cache: cache, backend: b}
 }
 
 // Read is a read callback
 func (idx *Index) Read(key Key) (Score, error) {
-	cacheKey := art.Key(key)
-	if score, ok := idx.cache.Search(cacheKey); ok {
-		return score.([]byte), nil
+	if score, ok := idx.cache.Get(key); ok {
+		return score, nil
 	}
 
 	score, err := idx.backend.Read(key)
 	if err == nil {
-		idx.cache.Insert(cacheKey, score)
+		idx.cache.Add(key, score)
 	}
 	return score, err
 }
@@ -36,11 +56,10 @@ func (idx *Index) Read(key Key) (Score, error) {
 // Write is a write callback
 func (idx *Index) Write(score Score) (Key, error) {
 	key := NewKey()
-	cacheKey := art.Key(key)
 	err := idx.backend.Write(key, score)
 	if err != nil {
 		return Key{}, err
 	}
-	idx.cache.Insert(cacheKey, score)
+	idx.cache.Add(key, score)
 	return key, nil
 }
